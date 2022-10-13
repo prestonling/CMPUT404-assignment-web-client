c3722d7a0db1353a2dbff7053d5d6013f7fa8f22
prestonling <pling@ualberta.ca>
2022-10-13 01:39:44 -0600

    Httpclient implementation passing all freetests

--- httpclient.py
+++ httpclient.py
@@ -24,6 +24,10 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+HTTP_VERSION_KEY = "Http-version"
+STATUS_CODE_KEY = "Status-code"
+REASON_PHRASE_KEY = "Reason-phrase"
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
@@ -37,17 +41,42 @@ class HTTPClient(object):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # remote_ip = get_remote_ip(host)
         self.socket.connect((host, port))
         return None
 
     def get_code(self, data):
-        return None
+        result = {}
+        request_split_result = data.split("\r\n\r\n")
+
+        header = request_split_result[0]
+        header_lines = header.split("\r\n")
+        status_line = header_lines[0].split()
+        
+        result[HTTP_VERSION_KEY] = status_line[0]
+        result[STATUS_CODE_KEY] = status_line[1]
+        result[REASON_PHRASE_KEY] = status_line[2]
+        return int(result[STATUS_CODE_KEY])
 
     def get_headers(self,data):
-        return None
+        request_split_result = data.split("\r\n\r\n")
+            
+        header = request_split_result[0]
+        header = header.split("\r\n", 1)[1]
+        return header
 
     def get_body(self, data):
-        return None
+        request_split_result = data.split("\r\n\r\n")
+        body = request_split_result[1]
+        
+        return body
+
+    def create_post_body(self, args):
+        body = ""
+        for key, value in args.items():
+            body += key + "=" + value + "&"
+        body = body[:-1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,13 +97,65 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+
+        o = urllib.parse.urlparse(url)
+        host = o.hostname
+        port = o.port
+        path = o.path
+        if path == "":
+            path = "/"
+        if port == None:
+            port = 80
+
+        self.connect(host, port)
+        request = f'GET {path} HTTP/1.1\r\n'
+        request += f'Host: {host}\r\n'
+        request += f'Connection: close\r\n\r\n'
+        
+        self.sendall(request)
+
+        response = self.recvall(self.socket)
+
+        self.close()
+
+        code = self.get_code(response)
+        self.get_headers(response)
+        body = self.get_body(response)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+
+        o = urllib.parse.urlparse(url)
+        host = o.hostname
+        port = o.port
+        path = o.path
+        if path == "":
+            path = "/"
+        if port == None:
+            port = 80
+
+        self.connect(host, port)
+        request = f'POST {path} HTTP/1.1\r\n'
+        request += f'Host: {host}\r\n'
+        request += f'Connection: close\r\n'
+        if args != None:
+            message_body = self.create_post_body(args)
+            message_body_length = len(message_body.encode('utf-8'))
+            request += f'Content-Length: {message_body_length}\r\n\r\n'
+            request += message_body
+        else:
+            request += f'Content-Length: 0\r\n\r\n'
+            request += "\r\n"
+
+        self.sendall(request)
+
+        response = self.recvall(self.socket)
+
+        self.close()
+
+        code = self.get_code(response)
+        self.get_headers(response)
+        body = self.get_body(response)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
